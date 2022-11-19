9e750aac926b25f0fef8efab75e9bbb78ed512f8
erwan-lacoudre <78564187+erwan-lacoudre@users.noreply.github.com>
2022-11-19 12:52:03 +0100

    Ajout description des blocs

--- data_analysis.py
+++ data_analysis.py
@@ -1,19 +1,24 @@
 import pandas as pd
 
-filename = 'transactions_202210.csv'
-
 def import_data(filename):
   raw_data = pd.read_csv(filename, header='infer', nrows=100)
   # print(raw_data)
-  print(raw_data.dtypes)
+  print(raw_data.dtypes) 
+  return raw_data
+
+
+if __name__ == '__main__':
+  transaction = 'transactions_202210.csv'
+  blocks = 'blocks_202210.csv'
+
+  transaction_data = import_data(transaction)
+  print(transaction_data['inputs'][0])
+  print(transaction_data['outputs'][0])
   """
     NOTE : ne pas utiliser le hashs ?
     hash -> id transaction, le simplifier ?
     block_hash -> utiliser block_number pour identifier blocs ?
     timestampS -> type object le convertir en type Date ?
   """
-  print(raw_data['outputs'][0])
 
-
-if __name__ == '__main__':
-  import_data(filename)
+  blocks_data = import_data(blocks)
