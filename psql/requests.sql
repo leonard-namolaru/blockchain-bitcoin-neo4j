f6eb521dc9ed6e575e3d565ec427ccd15deab520
erwan-lacoudre <78564187+erwan-lacoudre@users.noreply.github.com>
2023-01-04 00:48:52 +0100

    Dernier commit + ajout rapport

--- psql/requests.sql
+++ psql/requests.sql
@@ -1,4 +1,4 @@
-SELECT number, transaction_count 
+EXPLAIN ANALYSE SELECT number, transaction_count 
 FROM block 
 WHERE transaction_count = ( SELECT transaction_count
                             FROM block
@@ -17,3 +17,11 @@ WHERE transaction_count = ( SELECT transaction_count
                             ORDER BY transaction_count DESC
                             LIMIT 1 
                             );
+
+
+-- Faux si block contient 1 transaction non coinbase
+select count(from_cb) = 0
+from (select hash, is_coinbase from transaction where block_hash='00000000000000000005b7251fab086a1ee166cdae89ac17f706875a7ac0c74e' and is_coinbase='f') as from_cb;
+
+-- select count(in_block) from (select b.hash from block b where (select t.is_coinbase from transaction t where t.block_hash = b.hash)) as in_block;
+
